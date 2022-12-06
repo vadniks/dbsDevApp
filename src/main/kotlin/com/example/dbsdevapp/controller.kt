70049cf40774621ba93ae76964168cfa4c8ed4b4
Your Name <you@example.com>
2022-12-06 23:22:06 +0300

    Programming structure & logic

--- src/main/kotlin/com/example/dbsdevapp/controller.kt
+++ src/main/kotlin/com/example/dbsdevapp/controller.kt
@@ -23,8 +23,8 @@ class Controller(
     private val orderRepo: OrderRepo
 ) {
 
-    // curl 'localhost:8080/insert/component' -H 'Auth-credentials: admin:admin' -H 'Content-Type: application/json' -d '{"componentId":null,"name":"aa","type":1,"description":"bb","cost":10,"image":null,"count":1}'
-    @PostMapping("/insert$WHICH")
+    // curl 'localhost:8080/component' -H 'Auth-credentials: admin:admin' -H 'Content-Type: application/json' -d '{"componentId":null,"name":"aa","type":1,"description":"bb","cost":10,"image":null,"count":1}'
+    @PostMapping(WHICH)
     fun insert(
         @PathVariable which: String,
         @RequestHeader(AUTH_CREDENTIALS) credentials: String,
@@ -43,9 +43,9 @@ class Controller(
         }) responseOk else responseBadRequest
     }
 
-    // curl 'localhost:8080/get/component?id=1' -H 'Auth-credentials: admin:admin'
+    // curl 'localhost:8080/component?id=1' -H 'Auth-credentials: admin:admin'
     @ResponseBody
-    @GetMapping("/get$WHICH")
+    @GetMapping(WHICH)
     fun get(
         @PathVariable which: String,
         @RequestHeader(AUTH_CREDENTIALS) credentials: String,
@@ -62,9 +62,9 @@ class Controller(
         }?.json
     }
 
-    // curl 'localhost:8080/getAll/component' -H 'Auth-credentials: admin:admin'
+    // curl 'localhost:8080/all/component' -H 'Auth-credentials: admin:admin'
     @ResponseBody
-    @GetMapping("/getAll$WHICH")
+    @GetMapping("/all$WHICH")
     fun getAll(
         @PathVariable which: String,
         @RequestHeader(AUTH_CREDENTIALS) credentials: String
@@ -80,8 +80,8 @@ class Controller(
         }.map { it.json }
     }
 
-    // curl 'localhost:8080/update/component' -X PUT -H 'Auth-credentials: admin:admin' -H 'Content-Type: application/json' -d '{"componentId":2,"name":"aa","type":1,"description":"bb","cost":10,"image":null,"count":10}'
-    @PutMapping("/update$WHICH")
+    // curl 'localhost:8080/component' -X PUT -H 'Auth-credentials: admin:admin' -H 'Content-Type: application/json' -d '{"componentId":2,"name":"aa","type":1,"description":"bb","cost":10,"image":null,"count":10}'
+    @PutMapping(WHICH)
     fun update(
         @PathVariable which: String,
         @RequestHeader(AUTH_CREDENTIALS) credentials: String,
@@ -98,8 +98,8 @@ class Controller(
         }) responseOk else responseBadRequest
     }
 
-    // curl 'localhost:8080/delete/component?id=2' -X DELETE -H 'Auth-credentials: admin:admin'
-    @DeleteMapping("/delete$WHICH")
+    // curl 'localhost:8080/component?id=2' -X DELETE -H 'Auth-credentials: admin:admin'
+    @DeleteMapping(WHICH)
     fun delete(
         @PathVariable which: String,
         @RequestHeader(AUTH_CREDENTIALS) credentials: String,
